dcedb05e6a9af4a087695075e16062d315871979
Matthew-Shelby <matteo.maloni@student.univaq.it>
2025-12-07 13:16:37 +0100

    Prova innesto redis nel progetto...che Dio ce la mandi buona

---
+++ AVG_Project/src/body/main_body.py
@@ -0,0 +1,8 @@
+import time
+import redis
+
+print("🦾 [BODY] Avviato! In attesa di connessione Coppelia...", flush=True)
+
+# Loop infinito
+while True:
+    time.sleep(5)
---
+++ AVG_Project/src/brain/main_brain.py
@@ -0,0 +1,8 @@
+import time
+import redis
+
+print("🧠 [BRAIN] Avviato! In attesa di logica...", flush=True)
+
+# Loop infinito per tenere acceso il container
+while True:
+    time.sleep(5)
