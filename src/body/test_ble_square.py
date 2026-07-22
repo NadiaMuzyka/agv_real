483bc017105369ffa995cf7e0646e1d76a036c26
nadimuz <muzykanadia0@gmail.com>
2026-07-22 17:17:00 +0200

    batteria

--- src/body/test_ble_square.py
+++ src/body/test_ble_square.py
@@ -4,40 +4,14 @@
 
 # Want to draw a square?
 
-from irobot_edu_sdk.backend.bluetooth import Bluetooth
-from irobot_edu_sdk.robots import event, Create3
+from modules.connection.create3_connector import Create3Connector
 
-robot = Create3(Bluetooth())
+connector = Create3Connector()  # Si connette e resta bloccato finché non è pronto.
 
-@event(robot.when_play)
-async def go_to_dock(robot):
-    millivolt, percent = await robot.get_battery_level()
-    print(f'Battery level: {percent}% ({millivolt} mV)')
-    #print('Vado alla stazione di ricarica')
-    #result = await robot.dock()
-    #print(f'Risultato docking: {result}')
-    print('Fatto, mi disconnetto.')
-    try:
-        await robot.stop()
-        await robot.disconnect()
-        await robot._backend.disconnect()
-    except Exception as e:
-        # bleak/dbus-fast a volte sollevano EOFError (o simili) durante la
-        # disconnessione BLE da BlueZ: è un bug noto lato bleak, non nostro
-        # (es. https://github.com/hbldh/bleak/issues/1698). A questo punto
-        # il lavoro utile è già fatto, quindi ignoriamo e usciamo comunque.
-        print(f'Disconnessione BLE non pulita (ignorato): {e!r}')
-    finally:
-        # go_to_dock gira come task a sé (creato da _main()): un'eccezione
-        # qui non risalirebbe mai al try/except di robot.play() qui sotto,
-        # quindi il try/except sopra e questo finally devono stare per forza
-        # dentro la coroutine stessa.
-        robot._run = False  # Fa sì che _finished() salti la sua disconnessione.
-        robot._loop.stop()  # Ferma il loop così che play() ritorni da solo, senza Ctrl+C.
+millivolt, percent = connector.get_battery_level()
+print(f'Battery level: {percent}% ({millivolt} mV)')
+#print('Vado alla stazione di ricarica')
+#connector.dock()
 
-try:
-    robot.play()
-except RuntimeError:
-    # robot._loop.stop() ferma il loop prima che _main() finisca:
-    # asyncio solleva sempre questo errore in quel caso, ma è innocuo.
-    pass
+print('Fatto, mi disconnetto.')
+connector.disconnect()  # Disconnessione pulita, senza bisogno di Ctrl+C.
